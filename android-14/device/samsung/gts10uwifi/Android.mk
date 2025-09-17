a244a45279ff0bbda0e34c95d01c11e1c46e313a
mehranskystar <pavbearing1@gmail.com>
2025-09-17 18:49:28 +0330

    Create Android.mk
    Add Android.mk (based on TWRP standard)

---
+++ android-14/device/samsung/gts10uwifi/Android.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/gts10uwifi
+
+ifeq ($(TARGET_DEVICE),gts10uwifi)
+include $(call all-subdir-makefiles)
+endif
